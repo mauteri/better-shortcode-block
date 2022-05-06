ecad860f51bba703414e4d77b4ed6ffcecaefceb
Michael Auteri <mauteri@gmail.com>
2022-05-05 21:40:52 -0400

    Cleanup files a bit.

--- classes/class-assets.php
+++ classes/class-assets.php
@@ -1,16 +1,36 @@
 <?php
+/**
+ * @package better-shortcode-block
+ */
+
 namespace BetterShortcodeBlock;
 
+/**
+ * Class Assets.
+ */
 class Assets {
 
+	/**
+	 * Construct.
+	 */
 	public function __construct() {
 		$this->setup_hooks();
 	}
 
+	/**
+	 * Setup filters and actions.
+	 *
+	 * @return void
+	 */
 	protected function setup_hooks(): void {
 		add_action( 'enqueue_block_editor_assets', [ $this, 'block_editor_assets' ] );
 	}
 
+	/**
+	 * Enqueue block editor assets.
+	 *
+	 * @return void
+	 */
 	public function block_editor_assets(): void {
 		$path  = BETTER_SHORTCODE_BLOCK_PATH . '/assets/build/';
 		$build = BETTER_SHORTCODE_BLOCK_URL . 'assets/build/';
--- classes/class-setup.php
+++ classes/class-setup.php
@@ -15,7 +15,6 @@ class Setup {
 	 */
 	public function __construct() {
 		$this->instantiate();
-		$this->setup_hooks();
 	}
 
 	/**
@@ -26,30 +25,6 @@ class Setup {
 	protected function instantiate() {
 		new Assets();
 		new Rest_API();
-	}
-
-	/**
-	 * Setup actions and filters.
-	 *
-	 * @return void
-	 */
-	protected function setup_hooks() {
-		add_action( 'init', array( $this, 'register_block' ) );
-	}
-
-	public function register_block() {
-//		unregister_block_type( 'core/shortcode' );
-
-//		register_block_type(
-//			BETTER_SHORTCODE_BLOCK_PATH,
-//			array(
-//				'render_callback' => array( $this, 'render_shortcode' ),
-//			)
-//		);
-	}
-
-	public function render_shortcode( $attributes, $content ) {
-		return wpautop( $content );
 	}
 
 }
