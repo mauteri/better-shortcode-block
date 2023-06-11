0d2df7e6ed1d6f69ea8eb66668963c92d6c050fd
Mike Auteri <mauteri@gmail.com>
2023-06-11 11:21:01 -0400

    Add PHPUnit bootstrap and custom PHPCS ruleset

---
+++ tests/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * PHPUnit bootstrap file.
+ *
+ * @package better-shortcode-block
+ * @subpackage Tests
+ * @since 1.0.0
+ */
+
+// phpcs:disable Squiz.Commenting.FileComment.Missing
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$better_shortcode_block_bootstrap_instance = PMC\Unit_Test\Bootstrap::get_instance();
+
+tests_add_filter(
+	'plugins_loaded',
+	function() {
+		// Manually load our plugin without having to setup the development folder in the correct plugin folder.
+		require_once __DIR__ . '/../better-shortcode-block.php';
+	}
+);
+
+$better_shortcode_block_bootstrap_instance->start();
